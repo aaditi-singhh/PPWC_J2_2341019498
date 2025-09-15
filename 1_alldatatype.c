e61f21fea85decd70fda85b26f104c70697cf1b5
aaditi-singhh <aditisingh6841@gmail.com>
2025-09-15 20:55:48 +0530

    Create 1_alldatatype.c

---
+++ 1_alldatatype.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    int main(){
+    
+    int a;
+    float b;
+    char c;
+    
+    printf("enter a integer ");
+    scanf("%d",&a);
+    printf("integer is: %d \n",a);
+    
+    printf("enter a decimal number ");
+    scanf("%f",&b);
+    printf("float is: %f \n",b);
+    
+    printf("enter a character ");
+    scanf("%c",&c);
+    printf("character is: %c \n",c);
+
+
+    return 0;
+}
